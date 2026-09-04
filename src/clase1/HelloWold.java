722d26d6ba2d3f18edc859932b2f406457ef75a4
Saint <ss.mscx@gmail.com>
2026-09-03 19:26:23 -0600

    primer commit

---
+++ src/clase1/HelloWold.java
@@ -0,0 +1,11 @@
+package clase1;/*
+varias lineas de comentarios
+ */
+// hola mundo
+
+public class HelloWold {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/clase1/hellowordExercise1.java
@@ -0,0 +1,33 @@
+package clase1;
+
+public class hellowordExercise1 {
+    public static void main(String[] args) {
+        // es un printin donde describe mi nombre
+        System.out.println("Hola Martin");
+        // es un printin utilizando \n para que tenga un salto de linea
+        System.out.println("Hola\nmundo");
+        System.out.println("\n\nNombre: Martin \nEdad: 25\n Celeste\n");
+
+        // aca veremos los diferentes tipos de system que existen
+        //este funciona para marcar algun error
+        System.err.println("[ALERTA] ha ocurrido un error en la aplicación");
+
+        // para forzar el cierre del proyecto
+
+        System.out.println("Esta línea sí se va a imprimir.");
+
+        // Cerramos el programa con código 0 (finalización exitosa)
+        System.exit(0);
+
+        // El programa muere antes de llegar aquí:
+        System.out.println("Esta línea NUNCA se va a mostrar en pantalla.");
+
+        //Existen muchos otros pero habria que seguir investigando y ver la forma de aplicarlos
+
+        // con base al inciso donde cambiemos el nombre, le cambia el nombre a la clase automaticamente
+    }
+}
+
+/*
+cometario de varias líneas
+ */
