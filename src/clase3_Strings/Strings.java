11a064e0e0a9633467cac1646054e3c82e195737
Saint <ss.mscx@gmail.com>
2026-09-04 19:57:58 -0600

    Concatenación es la inicialización de la clase

--- src/clase3_Strings/Strings.java
+++ src/clase3_Strings/Strings.java
@@ -1,4 +1,14 @@
 package clase3_Strings;
 
 public class Strings {
+
+    public static void main(String[] args) {
+
+        // Cadenas de Texto
+        String name = "Mar";
+        var surname = new String("Mar");
+
+        // Concatenación
+        
+    }
 }
