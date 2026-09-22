5a63011489d9668a57ed95da584cab1dfc9a9771
Saint <ss.mscx@gmail.com>
2026-09-21 21:58:08 -0600

    condicion switch

--- src/clase4_condicionales/Conditionals.java
+++ src/clase4_condicionales/Conditionals.java
@@ -9,11 +9,32 @@ public class Conditionals {
         var age = 18;
 
         if (age > 18) {
-            System.out.println("El usuario es mayor de edad");
+            System.out.println("El usuario es mayor de edad\n");
         } else if (age == 18) {
-            System.out.println("El usuario a cumplido 18 años");
+            System.out.println("El usuario a cumplido 18 años\n");
         } else {
-            System.out.println("El usuario es menor de edad");
+            System.out.println("El usuario es menor de edad\n");
         }
+
+        // Condiciones "SWITCH"
+
+        var day = 1;
+
+        switch (day) {
+            case 1:
+                System.out.println("Lunes\n");
+                break;
+            case 2:
+                System.out.println("Martes\n");
+                break;
+            case 3:
+                System.out.println("Miercoles\n");
+                break;
+            default:
+                System.out.println("No es ningun día existente");
+
+        }
+
+
     }
 }
