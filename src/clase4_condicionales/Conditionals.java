909022aa7050ffa0da113abb818b2cbd34eea6c7
Saint <ss.mscx@gmail.com>
2026-09-21 21:50:17 -0600

    sentencia if subida

---
+++ src/clase4_condicionales/Conditionals.java
@@ -0,0 +1,19 @@
+package clase4_condicionales;
+
+public class Conditionals {
+    public static void main(String[] args) {
+
+        // Condicionales
+
+        // Condicion "IF"
+        var age = 18;
+
+        if (age > 18) {
+            System.out.println("El usuario es mayor de edad");
+        } else if (age == 18) {
+            System.out.println("El usuario a cumplido 18 años");
+        } else {
+            System.out.println("El usuario es menor de edad");
+        }
+    }
+}
