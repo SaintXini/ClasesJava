dd21e87ef53c0e063ff39be8bcda12bb1634ac44
Saint <ss.mscx@gmail.com>
2026-09-23 17:21:24 -0600

    subida de los array list

--- src/clase5_datos/List.java
+++ src/clase5_datos/List.java
@@ -21,13 +21,37 @@ public class List {
         // Añadir elementos
         // Nos percatamos que es dinamico
         names.add("Jose");
+        names.add("MarJose");
         names.add("Mar");
 
         System.out.println(names.size());
 
         // Acceder a los elementos
         System.out.println(names.getFirst());
+        System.out.println(names.get(1));
         System.out.println(names.getLast());
-        // 3.35
+
+        // Modificar los elementos de las listas
+
+        names.set(2, "ss.mscx@gmail.com");
+        System.out.println(names.getLast());
+
+        // Eliminar elementos de la lista
+
+        names.remove(2);
+       //  System.out.println(names.get(2)); // un Error ya no existe
+
+        // Buscar elementos
+        System.out.println(names.contains("MarJose"));
+        System.out.println(names.contains("Mar"));
+
+        // Limpiar toda la array list
+
+        names.clear();
+        System.out.println(names.size());
+
+//        Las principales diferencias es que el array tiene un limite especifico y las lsitas tienen varios espacios
+//         y Siempre se trabaja con objetos
+        
     }
 }
