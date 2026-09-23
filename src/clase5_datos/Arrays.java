f3c7419309ed358e5a8cda4ea8e353ce8f2b5df4
Saint <ss.mscx@gmail.com>
2026-09-22 23:42:29 -0600

    subida de la información de los arrays

--- src/clase5_datos/Arrays.java
+++ src/clase5_datos/Arrays.java
@@ -2,6 +2,35 @@ package clase5_datos;
 
 public class Arrays {
     public static void main(String[] args) {
-        // volver en el minuto 3.06
+
+        // como declarar los arrays y crearlos
+
+        int[] num = new int[3];
+
+        System.out.println(num);
+
+
+        String[] name = {"Martín", "Brais", "Xinico"};
+        System.out.println(name);
+
+        // Acceso, de que me vale tener muchos datos si no se como acceder a ellos
+
+        System.out.println(num[0]);
+        System.out.println(name[0]);
+
+        System.out.println((new String[3])[0]);
+
+        // Modificar los datos de los arrrays
+
+        num[0] = 1;
+        num[1] = 2;
+        num[2] = 3;
+
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+
+        // Null solo se aplica a los strings pero donde aplica los numeros no aplica
+
     }
 }
