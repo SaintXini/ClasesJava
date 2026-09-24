c22e13719bcd90e5577a88bdf0083e37b9a8d428
Saint <ss.mscx@gmail.com>
2026-09-23 21:47:53 -0600

    ciclo For

--- src/clase6_loops_o_bucles/Loops.java
+++ src/clase6_loops_o_bucles/Loops.java
@@ -2,6 +2,24 @@ package clase6_loops_o_bucles;
 
 public class Loops {
     public static void main(String[] args) {
-        
+
+        // Loops
+        // nos permite para repetid ejecuciónes concretas
+        // Nos suele servir para automatizar funciones
+
+
+        // 1er Bucle "For", es el mas habitual
+        // Controlado por un contador
+        // Siempe se marca cuanto se utiliza
+
+        for (int index = 0; index < 5; index++) {
+            System.out.println("hola, Java!");
+        }
+
+        String[] names = {"Martín", "Santiago", "Lala"};
+
+        for (int index = 0; index < names.length; index++) {
+            System.out.println(names[index]);
+        }
     }
 }
